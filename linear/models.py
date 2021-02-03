2ab349cdb7b9b8b695e56c730ba4b2ffe18f937c
Mirofil <kawgawr@gmail.com>
2021-02-03 10:04:15 +0100

    Implemented good support for parameterization of SOTL gradient - variable outer/inner training loop lengths. Should also be fairly ezy to get ValAcc outer loop going properly now

--- linear/models.py
+++ linear/models.py
@@ -3,8 +3,9 @@ from layers import Linear2, LinearMaxDeg
 
 class RegressionNet(torch.nn.Module):
     def __init__(self, num_features = 2, **kwargs):
-        super(Net, self).__init__()
+        super(RegressionNet, self).__init__()
         self.fc1 = torch.nn.Linear(num_features, 1, bias=False)
+        self.alphas = []
 
     def forward(self, x):
         return self.fc1(x)
@@ -14,16 +15,19 @@ class RegressionNet(torch.nn.Module):
             yield p
     
     def arch_params(self):
-        for p in [self.fc1.alphas]:
+        for p in [self.fc1.alphas] + self.alphas:
             yield p
 
 class SoTLNet(RegressionNet):
-    def __init__(self, num_features = 2, layer_type = "softmax_mult", **kwargs):
+    def __init__(self, num_features = 2, layer_type = "softmax_mult", weight_decay=0, **kwargs):
         super().__init__(**kwargs)
         if layer_type == "softmax_mult":
             self.fc1 = Linear2(num_features, 1, bias=False, **kwargs)
         elif layer_type == "max_deg":
             self.fc1 = LinearMaxDeg(num_features, 1, bias=False, **kwargs)
-
+        self.alphas = []
+        if weight_decay > 0:
+            self.weight_decay = torch.nn.Parameter(torch.tensor([weight_decay], dtype=torch.float32, requires_grad=True).unsqueeze(dim=0))
+            self.alphas = self.alphas.append(self.weight_decay)
     def forward(self, x, weight=None, alphas=None):
         return self.fc1(x, weight, alphas)
--- linear/sotl_utils.py
+++ linear/sotl_utils.py
@@ -4,105 +4,117 @@ from typing import *
 
 
 def sotl_gradient(
-    model, criterion, xs, ys, weight_buffer: Sequence, w_lr:float, T:int, order=1, hvp="exact", normalize_a_lr=True
+    model, criterion, xs, ys, weight_buffer: Sequence, w_lr:float, T:int, outer_loop_order=1,inner_loop_order=1, hvp="exact", normalize_a_lr=True, weight_decay_term=0, val_xs = None, val_ys=None
 ) -> Sequence:
     total_arch_gradient = None
-    weight_buffer = weight_buffer[-(min([len(weight_buffer)-len(xs),len(weight_buffer)- len(ys)])):]
+    recompute_top_level = None
+    loss = None
+    da=None
+    dw=None
     
 
-    if order is None or order <= 0:
-        order = min([len(weight_buffer), len(xs), len(ys)])
-        if len(weight_buffer) != len(xs):
-            print("SCERAM")
+    if inner_loop_order is None or inner_loop_order <= 0:
+        inner_loop_order = min([len(weight_buffer), len(xs), len(ys)])
+    if outer_loop_order is None or inner_loop_order <= 0:
+        outer_loop_order = min([len(weight_buffer), len(xs), len(ys)])
+
     if (
         len(weight_buffer) == 1
     ):  # the other branches won't work because we calculate gradients with weights at t-1 in them
         loss = criterion(model(xs[0], weight_buffer[0][0], model.fc1.alphas), ys[0])
         da = torch.autograd.grad(loss, model.arch_params(), retain_graph=True)
         total_arch_gradient = da
-    elif hvp == "exact":
-        for i in range(
-            len(weight_buffer) - 1, max(0, len(weight_buffer) - 1 - order), -1
-        ):
-            loss = criterion(model(xs[i], weight_buffer[i][0], model.fc1.alphas), ys[i])
-            da = torch.autograd.grad(loss, model.arch_params(), retain_graph=True)[0]
-            dw = torch.autograd.grad(loss, weight_buffer[i][0], retain_graph=True)[0]
-
-            loss2 = criterion(
-                model(xs[i], weight_buffer[i - 1][0], model.fc1.alphas), ys[i]
-            )
-            hessian_matrix = hessian(
-                loss2 * 1, weight_buffer[i - 1][0], model.fc1.alphas
-            ).reshape(
-                model.fc1.weight.size()[1], model.fc1.alphas.size()[1]
-            )  # TODO this whole line is WEIRD
-
-            second_order_term = torch.matmul(dw, hessian_matrix)
-
-            if total_arch_gradient is None:
-                total_arch_gradient = [0 for _ in range(len(da))]
-            for i, a_grad in enumerate(da):
-                total_arch_gradient[i] += a_grad + (
-                    -w_lr * second_order_term
-                )  # TODO this does not work if there are multiple arch parameter tensors! See the handling in finite diff code below
-
-    elif hvp == "finite_diff":
-        for i in range(
-            len(weight_buffer) - 1, max(0, len(weight_buffer) - 1 - order), -1
-        ):
-
-            loss = criterion(model(xs[i], weight_buffer[i][0], model.fc1.alphas), ys[i])
-            da = torch.autograd.grad(loss, model.arch_params(), retain_graph=True)
-            dw = torch.autograd.grad(loss, weight_buffer[i][0], retain_graph=True)
-
-            # Footnotes suggest to divide by L2 norm of the gradient
-            norm = torch.cat([w.view(-1) for w in dw]).norm()
-            eps = 0.0001 / norm
-
-            # w+ = w + eps*dw`
-            with torch.no_grad():
-                for p, d in zip(weight_buffer[i - 1], dw):
-                    p.add_(eps * d)
-
-            loss2 = criterion(
-                model(xs[i], weight_buffer[i - 1][0], model.fc1.alphas), ys[i]
-            )
-            dalpha_pos = torch.autograd.grad(
-                loss2, model.arch_params()
-            )  # dalpha { L_trn(w+) }
-
-            # w- = w - eps*dw`
-            with torch.no_grad():
-                for p, d in zip(weight_buffer[i - 1], dw):
-                    p.subtract_(2.0 * eps * d)
-            loss3 = criterion(
-                model(xs[i], weight_buffer[i - 1][0], model.fc1.alphas), ys[i]
-            )
-            dalpha_neg = torch.autograd.grad(
-                loss3, model.arch_params()
-            )  # dalpha { L_trn(w-) }
-
-            # recover w
-            with torch.no_grad():
-                for p, d in zip(weight_buffer[i - 1], dw):
-                    p.add_(eps * d)
-
-            second_order_term = [
-                -w_lr * (p - n) / (2.0 * eps) for p, n in zip(dalpha_pos, dalpha_neg)
-            ]
-            total_arch_gradient_local = [
-                da1 + da2 for (da1, da2) in zip(second_order_term, da)
-            ]
-            if total_arch_gradient is None:
-                total_arch_gradient = total_arch_gradient_local
-
-            else:
-                for g1, g2 in zip(total_arch_gradient, total_arch_gradient_local):
-                    g1.add_(g2)
     else:
-        raise NotImplementedError
+        # The outer loop equation is dSoTL/da = sum_{t=T-outer_loop_order)^T dL(w_t, alpha)/da
+        # The inner loop equation is dL(w_t, alpha)da = dL(w_t,alpha)/da + dL(w_t,alpha)/dw * -eta sum_{i=t-inner_loop_order}^t d^2L(w_i, alpha)dadw
+        for i in range(
+            len(weight_buffer) - 1, max(0, len(weight_buffer)-1-outer_loop_order), -1
+        ):
+            if (val_xs is not None) and (val_ys is not None):
+                top_level_x = val_xs[0]
+                top_level_y = val_ys[0]
+                
+            else:
+                top_level_x = xs[i]
+                top_level_y = ys[i]
+
+
+            loss = criterion(model(top_level_x, weight_buffer[i][0], model.fc1.alphas), top_level_y)
+            da = [x for x in torch.autograd.grad(loss, model.arch_params(), retain_graph=True, allow_unused=True) if x is not None]
+            dw = torch.autograd.grad(loss, weight_buffer[i][0], retain_graph=True)
+            if hvp == "exact":
+                
+                for j in range(i, max(0, i - inner_loop_order), -1):
+                    loss2 = criterion(
+                        model(xs[i], weight_buffer[j - 1][0], model.fc1.alphas), ys[i]
+                    )
+                    hessian_matrix = hessian(
+                        loss2 * 1, weight_buffer[j - 1][0], model.fc1.alphas
+                    ).reshape(
+                        model.fc1.weight.size()[1], model.fc1.alphas.size()[1]
+                    )  # TODO this whole line is WEIRD. It should be made more general to allow multiple alphas ass well
+
+                    second_order_term = torch.matmul(dw[0], hessian_matrix)
+
+                    if total_arch_gradient is None:
+                        total_arch_gradient = [0 for _ in range(len(da))]
+                    for k, a_grad in enumerate(da):
+
+                        total_arch_gradient[k] += a_grad + (
+                            -w_lr * second_order_term
+                        )  # TODO this does not work if there are multiple arch parameter tensors! See the handling in finite diff code below
+
+            elif hvp == "finite_diff":
+                
+                for j in range(i, 0, -1):
+                    # DARTS footnotes suggest to divide by L2 norm of the gradient
+                    norm = torch.cat([w.view(-1) for w in dw]).norm()
+                    eps = 0.0001 / norm
+
+                    # w+ = w + eps*dw`
+                    with torch.no_grad():
+                        for p, d in zip(weight_buffer[j - 1], dw):
+                            p.add_(eps * d)
+
+                    loss2 = criterion(
+                        model(xs[j], weight_buffer[j - 1][0], model.fc1.alphas), ys[j]
+                    )
+                    dalpha_pos = torch.autograd.grad(
+                        loss2, model.arch_params()
+                    )  # dalpha { L_trn(w+) }
+
+                    # w- = w - eps*dw`
+                    with torch.no_grad():
+                        for p, d in zip(weight_buffer[j - 1], dw):
+                            p.subtract_(2.0 * eps * d)
+                    loss3 = criterion(
+                        model(xs[j], weight_buffer[j - 1][0], model.fc1.alphas), ys[j]
+                    )
+                    dalpha_neg = torch.autograd.grad(
+                        loss3, model.arch_params()
+                    )  # dalpha { L_trn(w-) }
+
+                    # recover w
+                    with torch.no_grad():
+                        for p, d in zip(weight_buffer[j - 1], dw):
+                            p.add_(eps * d)
+
+                    second_order_term = [
+                        -w_lr * (p - n) / (2.0 * eps) for p, n in zip(dalpha_pos, dalpha_neg)
+                    ]
+                    total_arch_gradient_local = [
+                        da1 + da2 for (da1, da2) in zip(second_order_term, da)
+                    ]
+                    if total_arch_gradient is None:
+                        total_arch_gradient = total_arch_gradient_local
+
+                    else:
+                        for g1, g2 in zip(total_arch_gradient, total_arch_gradient_local):
+                            g1.add_(g2)
+            else:
+                raise NotImplementedError
 
     if normalize_a_lr:
         for g in total_arch_gradient:
-            g.multiply_(T/order)
+            g.multiply_(T/inner_loop_order)
     return total_arch_gradient
