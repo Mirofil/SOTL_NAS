Trying to fix gradients for Luketina reproduction but its not working great. Might have to rethink my strategy

linear/models.py
@@ -69,11 +69,12 @@ class MLP(RegressionNet):
         self._input_dim = input_dim
         self.lin1 = nn.Linear(input_dim, hidden_dim)
         self.lin2 = nn.Linear(hidden_dim, hidden_dim)
+        self.lin22 = nn.Linear(hidden_dim, hidden_dim)
+        self.lin23 = nn.Linear(hidden_dim, hidden_dim)
+
         self.lin3 = nn.Linear(hidden_dim, output_dim)
-        self.alphas = []
         if weight_decay > 0:
             self.alpha_weight_decay = torch.nn.Parameter(torch.tensor([weight_decay], dtype=torch.float32, requires_grad=True).unsqueeze(dim=0))
-            self.alphas.append(self.alpha_weight_decay)
         else:
             self.alpha_weight_decay = 0
 
@@ -91,6 +92,10 @@ class MLP(RegressionNet):
         x = x.view(-1, self._input_dim)
         x = F.relu(self.lin1(x))
         x = F.relu(self.lin2(x))
+        # x = F.relu(self.lin22(x))
+        # x = F.relu(self.lin23(x))
+
+
         x = self.lin3(x)
 
         # with torch.no_grad():

linear/sotl_utils.py
@@ -74,7 +74,7 @@ def sotl_gradient(
         
         # OUTER LOOP
         for i in range(
-            len(weight_buffer) - 1, max(0, len(weight_buffer)-1-grad_outer_loop_order), -1
+            len(weight_buffer) - 2, max(0, len(weight_buffer)-2-grad_outer_loop_order), -1
         ):
             if (val_xs is not None) and (val_ys is not None):
                 top_level_x = val_xs[0]
@@ -146,12 +146,17 @@ def sotl_gradient(
                     if model.alpha_weight_decay > 0:
                         for weight in weight_buffer[j - 1]:
                             param_norm = param_norm + torch.pow(weight.norm(2), 2)
+
+                    old_weights = switch_weights(model, weight_buffer[j-1])
+
                     loss2 = criterion(
                         model(x, weight_buffer[j - 1], model.arch_params()), y
                     ) + param_norm*model.alpha_weight_decay
                     dalpha_pos = [x for x in torch.autograd.grad(
                         loss2, model.arch_params(), allow_unused=True
                     ) if x is not None]  # dalpha { L_trn(w+) }
+
+                    no_longer_needed_weights = switch_weights(model, old_weights)
 
                     # w- = w_{t-1} - eps*dL(w_t,alpha)dw
                     with torch.no_grad():
@@ -162,12 +167,15 @@ def sotl_gradient(
                     if model.alpha_weight_decay > 0:
                         for weight in weight_buffer[j - 1]:
                             param_norm = param_norm + torch.pow(weight.norm(2), 2)
+                    old_weights = switch_weights(model, weight_buffer[j-1])
+
                     loss3 = criterion(
                         model(x, weight_buffer[j - 1], model.arch_params()), y
                     ) + param_norm*model.alpha_weight_decay
                     dalpha_neg = [x for x in torch.autograd.grad(
                         loss3, model.arch_params(), allow_unused=True
                     ) if x is not None]  # dalpha { L_trn(w-) }
+                    no_longer_needed_weights = switch_weights(model, old_weights)
 
                     # recover w
                     with torch.no_grad():

linear/train.py
@@ -77,11 +77,13 @@ def train_bptt(
             )
 
             weight_buffer = WeightBuffer(T=T, checkpoint_freq=w_checkpoint_freq)
-            for intra_batch_idx, (x, y) in enumerate(zip(xs, ys)):
+            weight_buffer.add(model, 0)
+
+            for intra_batch_idx, (x, y) in enumerate(zip(xs, ys),1):
                 x = x.to(device)
                 y = y.to(device)
 
-                weight_buffer.add(model, intra_batch_idx)
+                # weight_buffer.add(model, intra_batch_idx) # TODO Should it be added here?
 
                 y_pred = model(x)
 
@@ -89,6 +91,8 @@ def train_bptt(
                 if extra_weight_decay is not None and extra_weight_decay != 0:
                     for weight in model.weight_params():
                         param_norm = param_norm + torch.pow(weight.norm(2), 2)
+                    param_norm = torch.multiply(model.alpha_weight_decay, param_norm)
+                # print(param_norm)
                 
                 
                 loss = criterion(y_pred, y) + param_norm
@@ -106,6 +110,7 @@ def train_bptt(
 
                 w_optimizer.step()
                 w_optimizer.zero_grad()
+                weight_buffer.add(model, intra_batch_idx)
 
                 true_batch_index += 1
                 wandb.log(
@@ -174,6 +179,8 @@ def train_bptt(
                     if log_alphas:
                         if hasattr(model, "fc1") and hasattr(model.fc1, "degree"):
                             wandb.log({"Alpha":model.fc1.degree.item()})
+                        if hasattr(model,"alpha_weight_decay"):
+                            wandb.log({"Alpha": model.alpha_weight_decay.item()})
 
                     a_optimizer.zero_grad()
 
@@ -198,8 +205,6 @@ def valid_func(model, dset_val, criterion, device = 'cuda' if torch.cuda.is_avai
     val_meter = AverageMeter()
     val_acc_meter = AverageMeter()
 
-    correct = 0
-    total = 0.0000000000001 # to avoid potential division by zero alter
     with torch.no_grad():
         for batch in val_loader:
             x, y = batch
@@ -272,11 +277,11 @@ def main(num_epochs = 50,
     D = 18,
     N = 50000,
     w_lr = 1e-4,
-    w_momentum=0.9,
-    w_weight_decay=0.1,
+    w_momentum=0.0,
+    w_weight_decay=0.0,
     a_lr = 3e-4,
-    a_momentum = 0.9,
-    a_weight_decay = 0.1,
+    a_momentum = 0.0,
+    a_weight_decay = 0.0,
     T = 10,
     grad_clip = 1,
     logging_freq = 200,
@@ -286,16 +291,16 @@ def main(num_epochs = 50,
     featurize_type="fourier",
     initial_degree=1,
     hvp="finite_diff",
-    arch_train_data="sotl",
+    arch_train_data="val",
     normalize_a_lr=True,
     w_warm_start=0,
-    extra_weight_decay=0.0,
-    grad_inner_loop_order=-1,
+    extra_weight_decay=0.1e-6,
+    grad_inner_loop_order=1,
     grad_outer_loop_order=-1,
     model_type="MNIST",
     dataset="MNIST",
     device= 'cuda' if torch.cuda.is_available() else 'cpu',
-    train_arch=False
+    train_arch=True
     ):
     config = locals()
 
@@ -391,9 +396,9 @@ N = 50000
 w_lr = 1e-3
 w_momentum=0.9
 w_weight_decay=0.0
-a_lr = 3e-3
+a_lr = 3e-4
 a_momentum = 0.9
-a_weight_decay = 0.2
+a_weight_decay = 0.0
 T = 10
 grad_clip = 1
 logging_freq = 200
@@ -407,7 +412,7 @@ normalize_a_lr=True
 w_warm_start=0
 log_grad_norm=True
 log_alphas=False
-extra_weight_decay=0.15
+extra_weight_decay=0.0001
 grad_inner_loop_order=-1
 grad_outer_loop_order=-1
 arch_train_data="val"