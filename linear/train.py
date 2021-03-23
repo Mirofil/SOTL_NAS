c9b1ee9e2f0a4cd421de0c1915c8bc76e66abd65
Mirofil <kawgawr@gmail.com>
2021-03-23 22:45:12 +0100

    All is gucci

--- linear/train.py
+++ linear/train.py
@@ -6,7 +6,7 @@
 # python linear/train.py --model_type=max_deg --dataset=sklearn_friedman1 --dry_run=False --T=1 --a_weight_decay=0.1 --grad_outer_loop_order=1 --grad_inner_loop_order=1 --mode=bilevel --device=cpu --optimizer_mode=autograd --n_samples=50000  --epochs=1
 
 # python linear/train.py --model_type=MNIST --dataset=MNIST --dry_run=False --T=1 --w_warm_start=0 --grad_outer_loop_order=-1 --grad_inner_loop_order=-1 --mode=bilevel --device=cuda --extra_weight_decay=0.0001 --w_weight_decay=0 --arch_train_data=val
-# python linear/train.py --model_type=max_deg --epochs 1 --steps_per_epoch=1 --dataset=fourier --dry_run=True --grad_outer_loop_order=-1 --grad_inner_loop_order=-1 --mode=bilevel --device=cpu --ihvp=exact --inv_hess=exact --hvp=exact --rand_seed 1 --arch_train_data sotl --optimizer_mode=manual --T=25 --recurrent True --w_lr=1e-1 --a_lr=1e-3
+# python linear/train.py --model_type=max_deg --epochs 20 --steps_per_epoch=1 --dataset=fourier --dry_run=True --grad_outer_loop_order=-1 --grad_inner_loop_order=-1 --mode=bilevel --device=cpu --ihvp=exact --inv_hess=exact --hvp=exact --rand_seed 1 --arch_train_data sotl --optimizer_mode=manual --T=25 --recurrent True --w_lr=1e-1 --a_lr=1e-3 --adaptive_a_lr=True
 # python linear/train.py --model_type=rff_bag --epochs 50 --dataset=MNISTrff --dry_run=True --grad_outer_loop_order=-1 --grad_inner_loop_order=-1 --mode=bilevel --device=cpu --ihvp=exact --inv_hess=exact --hvp=exact --rand_seed 1 --arch_train_data sotl --optimizer_mode=autograd --loss=ce --T=2 --recurrent True --a_weight_decay 0 --a_lr=1500000000000 --w_weight_decay 0.0001 --train_arch=True --w_lr=1
 # python linear/train.py --model_type=rff_bag --epochs 300 --dataset=MNISTrff --dry_run=True --grad_outer_loop_order=-1 --grad_inner_loop_order=-1 --mode=bilevel --device=cuda --ihvp=exact --inv_hess=exact --hvp=exact --rand_seed 1 --arch_train_data sotl --optimizer_mode=autograd --loss=ce --T=2 --recurrent True --a_weight_decay 0 --a_lr=1 --w_weight_decay 0.01 --train_arch=True --w_lr=10
 #pip install --force git+https://github.com/Mirofil/pytorch-hessian-eigenthings.git
@@ -104,8 +104,12 @@ def main(epochs = 50,
     bilevel_w_steps=None,
     debug=False,
     recurrent=True,
-    l=1e5
+    l=1e5,
+    adaptive_a_lr=False
     ):
+    if adaptive_a_lr is True:
+        a_lr = a_lr*(T**(1/2))
+
     config = locals()
     if dry_run:
         os.environ['WANDB_MODE'] = 'dryrun'
