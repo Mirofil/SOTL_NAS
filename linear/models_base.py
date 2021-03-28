be696522f38fa09476f8f2264a9dbc9079524d7a
Mirofil <kawgawr@gmail.com>
2021-03-28 15:05:46 +0200

    Forgot to add file

---
+++ linear/models_base.py
@@ -0,0 +1,29 @@
+import torch
+
+class Hypertrainable(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(Hypertrainable, self).__init__()
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def named_weight_params(self):
+        for n,p in self.named_parameters():
+            if 'alpha' not in n:
+                yield (n,p)
+            else:
+                continue
+    
+    def weight_params(self):
+        for n,p in self.named_parameters():
+            if 'alpha' not in n:
+                yield p
+            else:
+                continue
+    
+    def arch_params(self):
+        for n,p in self.named_parameters():
+            if 'alpha' in n and p.requires_grad:
+                yield p
+            else:
+                continue
