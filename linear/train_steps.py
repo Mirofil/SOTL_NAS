0c63daa2c8693e742566b2d9be61b330de0a40b9
Mirofil <kawgawr@gmail.com>
2021-03-29 10:55:44 +0200

    Bug in model arch reject logging

--- linear/train_steps.py
+++ linear/train_steps.py
@@ -218,16 +218,17 @@ def arch_step(model, criterion, xs, ys, weight_buffer, w_lr, hvp, inv_hess, ihvp
         arch_coef = clip_grad_raw(total_arch_gradient, grad_clip)
         # print(arch_coef)
 
-    if not hasattr(model, "arch_reject_count"):
-        model.arch_reject_count = 0
-    else:
-        model.arch_reject_count += 1
+
 
     with torch.no_grad():
         for (w_name, w), da in zip(model.named_arch_params(), total_arch_gradient):
             if "alpha_lr" in w_name and (w-model.cfg["a_lr"]*da).item() < 0:
                 print(f"Override update! Instead of {(w-model.cfg['a_lr']*da).item()}, we get {w.item()/2}")
                 w.multiply_(1/2)
+            if not hasattr(model, "arch_reject_count"):
+                model.arch_reject_count = 0
+            else:
+                model.arch_reject_count += 1
             else:
                 w.subtract_(other=da, alpha=model.cfg["a_lr"])
                 if (w.item()) < 0:
